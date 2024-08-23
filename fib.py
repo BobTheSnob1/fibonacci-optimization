c5d3abfc22cd0506fc20e0931be847ffc51b93d4
Richard <richitdd@gmail.com>
2024-08-23 20:13:38 +0200

    pep8 compliance

--- fib.py
+++ fib.py
@@ -1,25 +1,28 @@
 import time
 from numpy import average
 
+
 def fib(n):
     if n < len(fibs):
         return fibs[n]
     fibs.append(fibs[n - 1] + fibs[n - 2])
     return fibs[n]
 
+
 def fib_timeboxed():
     start_time = time.time()
     n = 0
     fib_sequence = []
-    
+
     while True:
         elapsed_time = time.time() - start_time
         if elapsed_time >= 0.1:
             break
         fib_sequence.append(fib(n))
         n += 1
-    
+
     return n
+
 
 if __name__ == "__main__":
     fib_reached = []
@@ -27,7 +30,8 @@ if __name__ == "__main__":
         fibs = [1, 1]
         fib_reached.append(fib_timeboxed())
 
-    print(f"Fibonacci sequence calculated 10 times for 0.1s each. Highest ns reached:")
+    print("Fibonacci sequence calculated 10 times for 0.1s each. " +
+          "Highest ns reached:")
     for _ in fib_reached:
         print(_)
     print(f"Mean highest n reached: {average(fib_reached):.0f}")
